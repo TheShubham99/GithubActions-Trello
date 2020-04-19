3079b453944d74bc39b222466e2c6f763f43a198
Prathamesh Sahasrabhojane <ssahasrabhojane@gmail.com>
2020-04-19 16:21:05 +0530

    adding j query

--- actions/create_issue.js
+++ actions/create_issue.js
@@ -1,10 +1,26 @@
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = "http://code.jquery.com/jquery-2.2.1.min.js";
+
+// Then bind the event to the callback function.
+// There are several events for cross browser compatibility.
+script.onreadystatechange = handler;
+script.onload = handler;
+
+// Fire the loading
+head.appendChild(script);
+
+function handler(){
+   console.log('jquery added :)');
+}
+
+
+
 const actor = process.env.GITHUB_ACTOR;
 const trello_api_key = process.env.TRELLO_API_KEY;
 const trello_api_token = process.env.TRELLO_API_TOKEN;
 const action_no = process.env.GITHUB_RUN_NUMBER;
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
 
 function newIssue(cardName,Description){
     $.post("https://api.trello.com/1/cards",
